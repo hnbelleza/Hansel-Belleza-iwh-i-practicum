5332832c81c3b9a629dc2ba994ba8fae341ca693
cracker$ <hanselbelleza@gmail.com>
2023-11-29 21:33:05 +0800

    update pug

--- index.js
+++ index.js
@@ -38,6 +38,15 @@ app.get('/', async (req, res) => {
 
 // * Code for Route 2 goes here
 
+app.get('/updates', async (req, res) => {
+    try {
+        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum.' });      
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
